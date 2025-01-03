ea061b047b0741a00681712e51dd762950067f16
Roman Campbell <Roman_Campbell@lawb.uscourts.gov>
2025-01-03 08:05:34 -0600

    Database work

--- api/api.py
+++ api/api.py
@@ -2,19 +2,38 @@ import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import request
+import os
+
+db_path = os.path.join(os.path.dirname(__file__), "db", "pruchases.db")
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/sendData', methods=['POST'])
-def sendDataResponse():
-    incoming_data = request.get_json()
+def addPurchaseRequest():
+    if request.method == 'POST':
+        data = request.json
+        if not data:
+            return jsonify({"error": "Invalid data"}), 400
 
-    print("Incoming Data:", incoming_data)
-
-    # Respond with a success message
-    return jsonify({"message": "Data received successfully", "received": incoming_data})
-
+    # Break data up into variables
+    id = data.get("id")
+    requester = data.get("requester")
+    phone_ext = data.get("phone_ext")
+    date_request = data.get("date_request")
+    date_needed = data.get("date_needed")
+    order_type = data.get("order_type")
+    file_attachments = data.get("file_attachments")
+    item_description = data.get("item_description")
+    justification = data.get("justification")
+    addition_comments = data.get("addition_comments")
+    training_not_aval = data.get("training_not_aval")
+    needs_not_meet = data.get("needs_not_meet")
+    budget_obj_code = data.get("budget_obj_code")
+    fund = data.get("fund")
+    price = data.get("price")
+    location = data.get("location")
+    quantity = data.get("quantity")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- api/purchase_app_database.py
+++ api/purchase_app_database.py
@@ -1,14 +1,62 @@
 import sqlite3
+import os
 
-# Connect to the database
-con = sqlite3.connect("./db/purchases.db")
-cursor = connection.cursor()
 
-# SQLLITE 3 DATABASE CREATION 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS purchase_requests (
-               id INTEGER PRIMARY KEY,
-               requester TEXT NOT NULL
-               )
+# Get database connection
+def getDBConnection(db):
+    connection = sqlite3.connect(db)
+    connection.row_factory = sqlite3.Row
+    return connection
+    
+# Will only create if not present
+def createDB(connection):
+    
+    # SQLLITE 3 DATABASE CREATION 
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS purchase_requests (
+                id INTEGER PRIMARY KEY NOT NULL,
+                requester TEXT NOT NULL,
+                phone_ext INTEGER NOT NULL,
+                date_request TEXT,
+                date_needed TEXT,
+                order_type TEXT,
+                file_attachments BLOB,
+                item_description TEXT,
+                justification TEXT,
+                addition_comments TEXT,
+                training_not_aval TEXT,
+                needs_not_meet TEXT,
+                budget_obj_code TEXT,
+                fund TEXT,
+                price REAL,
+                location TEXT,
+                quantity INTEGER,
+                )
+    """)
 
-""")
+    connection.commit()
+    connection.close()
+
+# Function for inserting purchase request data into database
+def insertPurchaseReq(id, requester, phone_ext, date_request, date_needed, order_type, 
+                      file_attachments, item_desc, justification, addition_comments, 
+                      training_not_aval, needs_not_meet, budget_obj_code, fund, price, location, quantity):
+    connection = getDBConnection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("""
+        INSERT INTO purchase_requests               
+        (id, requester, phone_ext, date_request, date_needed, order_type, 
+                      file_attachments, item_desc, justification, addition_comments, 
+                      training_not_aval, needs_not_meet, budget_obj_code, fund, price, location, quantity)
+        VALUES
+        (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)        
+        """, (id, requester, phone_ext, date_request, date_needed, order_type, 
+                      file_attachments, item_desc, justification, addition_comments, 
+                      training_not_aval, needs_not_meet, budget_obj_code, fund, price, location, quantity))
+        connection.commit()
+        print("Purchase request inserted successfully")
+    except sqlite3.Error as e:
+        print("Error inserting purchase request: ", e)
+    finally:
+        connection.close()
