d4350196691847246a15f868b3c13136c3a388ac
Roman Campbell <Roman_Campbell@lawb.uscourts.gov>
2025-01-03 10:42:23 -0600

    api receiving data

--- api/api.py
+++ api/api.py
@@ -1,39 +1,55 @@
-import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask import request
+import json
 import os
+import threading
 
 db_path = os.path.join(os.path.dirname(__file__), "db", "pruchases.db")
 
 app = Flask(__name__)
 CORS(app)
 
+# Dictionary for sql column
+purchase_cols = {
+    'id': None,
+    'requester': None,
+    'phoneext': None,
+    'datereq': None,
+    'dateneed': None,
+    'orderType': None,
+    'fileAttachments': None,
+    'itemDescription': None,
+    'justification': None,
+    'addComments': None,
+    'learnAndDev': None,
+    'budgetObjCode': None,
+    'fund': None,
+    'price': None,
+    'location': None,
+    'quantity': None
+}
+
 @app.route('/sendData', methods=['POST'])
-def addPurchaseRequest():
+def getPurchaseRequest():
     if request.method == 'POST':
         data = request.json
+        
         if not data:
-            return jsonify({"error": "Invalid data"}), 400
-
-    # Break data up into variables
-    id = data.get("id")
-    requester = data.get("requester")
-    phone_ext = data.get("phone_ext")
-    date_request = data.get("date_request")
-    date_needed = data.get("date_needed")
-    order_type = data.get("order_type")
-    file_attachments = data.get("file_attachments")
-    item_description = data.get("item_description")
-    justification = data.get("justification")
-    addition_comments = data.get("addition_comments")
-    training_not_aval = data.get("training_not_aval")
-    needs_not_meet = data.get("needs_not_meet")
-    budget_obj_code = data.get("budget_obj_code")
-    fund = data.get("fund")
-    price = data.get("price")
-    location = data.get("location")
-    quantity = data.get("quantity")
-
+            return jsonify({"error": "Invalid data"}), 400  
+    
+    for item in data['dataBuffer']:
+        if isinstance(item, dict):
+            for k,v in item.items():
+                print(f"{k}: {v}")
+                
+    return jsonify(data)
+       
+    
 if __name__ == "__main__":
     app.run(debug=True)
+    data = getPurchaseRequest() 
+    
+        
+    
+        
+    
