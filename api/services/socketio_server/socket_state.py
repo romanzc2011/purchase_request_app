b7809c4d45caaab08263eb4442a3219fe3645634
romanzc2011 <93228839+romanzc2011@users.noreply.github.com>
2025-09-09 12:00:10 -0500

    update desc

--- api/services/socketio_server/socket_state.py
+++ api/services/socketio_server/socket_state.py
@@ -1,5 +1,15 @@
 from collections import defaultdict
 
+"""
+AUTHOR: ROMAN CAMPBELL
+DATE: 09/09/2025
+SOCKET STATE -- tracks socketio state
+
+This is necessary to map sid to users to prevent race condition like behavior. Executing
+an sio.emit will broadcast to all connected clients. This will cause issues if multiple
+users make requests at the sametime. This way each emit will send only to it mapped sid.
+"""
+
 # Map username -> sids
 user_sids = defaultdict(set)
 
